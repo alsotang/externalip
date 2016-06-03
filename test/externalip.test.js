bb95f4bf25eda7bfe876c4da94ddd7a3a802bf65
alsotang <alsotang@gmail.com>
2016-06-03 10:56:18 +0800

    use ip.cn

--- test/externalip.test.js
+++ test/externalip.test.js
@@ -5,6 +5,7 @@ describe('externalip.test.js', function () {
   this.timeout(10 * 1000);
   it('should return external ip', function (done) {
     externalip(function (err, ip) {
+      console.log('your ip', ip)
       ip.should.match(/^\d+.\d+.\d+.\d+$/);
       done(err);
     });
